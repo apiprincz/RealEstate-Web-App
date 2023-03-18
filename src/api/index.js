c62fd7fc61f62178851cf6052b25c991b5825b64
DCodeBeast <princzpharell@gmail.com>
2023-03-18 21:59:57 +0100

    update agent page

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// const API = axios.create({baseURL:"https://ilenla-backend.onrender.com"})
-const API = axios.create({baseURL:"http://localhost:8000"})
+const API = axios.create({baseURL:"https://ilenla-server.onrender.com/"})
+// const API = axios.create({baseURL:"http://localhost:8000"})
 
 API.interceptors.request.use((req)=> {
     if(localStorage.getItem("profile")){
